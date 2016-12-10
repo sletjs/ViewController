5c5e8bb78b0ff9cdec138a156a63627cd617b399
i5ting <i5ting@126.com>
2016-12-10 11:57:05 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 'use strict';
+const views = require('koa-views');
 
 module.exports = class ViewController {
-  constructor(ctx, next) {
+  constructor(app, ctx, next) {
+    this.app = app
     this.ctx = ctx
     this.query = ctx.query
     this.next = next
     this.tpl = ''
     this.data = {}
     
+    // 定义中间件
+    var _views = views(this.viewPath, this.opts.views.option)
+    this.app.defineMiddleware('koa-views', _views)
+    
+    // 定义global filter
     this.global_filter = ['koa-bodyparser', 'koa-views']
+    
+    // export
   }
 }
-  
